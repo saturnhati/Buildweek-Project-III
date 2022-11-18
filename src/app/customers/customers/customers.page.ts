d0099143d2e9d8bee83a016f5adec921e4dec561
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-11-18 16:23:00 +0100

    fixed add invoice / have to fix data in invoice list

--- src/app/customers/customers/customers.page.ts
+++ src/app/customers/customers/customers.page.ts
@@ -3,7 +3,8 @@ import { NgForm } from '@angular/forms';
 import { AuthService, AuthData } from 'src/app/auth/auth.service';
 import { User } from 'src/app/auth/user.interface';
 import { CustomersService } from '../customers.service';
-import { IClient } from '../interface-invoice.interface';
+import { IClient, InterfaceInvoice } from '../interface-invoice.interface';
+import { InvoicesService } from '../invoices.service';
 
 @Component({
   templateUrl: './customers.page.html',
@@ -13,13 +14,16 @@ export class CustomersPage implements OnInit {
   list: boolean = true;
   @ViewChild('f') mioForm!: NgForm;
   customersArr: IClient[] = [];
+  invoiceArr: InterfaceInvoice[] = [];
   error: undefined;
   customersObj!: IClient | null;
   loggedUser!: AuthData | null;
+  data: string = new Date().toISOString().slice(0, 10);
 
   constructor(
     private customersService: CustomersService,
-    private authService: AuthService
+    private authService: AuthService,
+    private invoicesService: InvoicesService
   ) {}
 
   ngOnInit(): void {
@@ -55,10 +59,8 @@ export class CustomersPage implements OnInit {
     let index = this.customersArr.indexOf(customer);
     this.customersArr.splice(index, 1);
   }
-  showDetails(custumer: IClient) {
-    this.customersObj = custumer;
-    console.log(this.customersObj);
-    console.log(custumer);
+  showDetails(customer: IClient) {
+    this.customersObj = customer;
   }
   getCustomer() {
     this.customersService
@@ -67,5 +69,28 @@ export class CustomersPage implements OnInit {
   }
   logout(user: AuthData) {
     this.authService.logout();
+  }
+
+  showForm(customer: IClient) {
+    this.customersObj = customer;
+  }
+
+  addInvoice() {
+    let obj: InterfaceInvoice = this.mioForm.value;
+    obj.dataInserimento = JSON.stringify(Date.now());
+    obj.dataUltimaModifica = JSON.stringify(Date.now());
+    if (this.customersObj !== null) {
+      obj.cliente = this.customersObj;
+    }
+    obj.anno = new Date().getFullYear();
+    this.invoicesService.addInvoice(obj).subscribe(
+      (data) => {
+        this.invoiceArr.push(data);
+      },
+      (err) => {
+        (this.error = err), console.log(this.error);
+      }
+    );
+    this.mioForm.reset();
   }
 }
